b0f087c607cfee1522b30a67c2efec537e2469cd
Patrick Kimber <code@pkimber.net>
2014-12-12 14:24:51 +0100

    version 0.1.12

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/enquiry/*.*',
         ],
     },
-    version='0.1.11',
+    version='0.1.12',
     description='enquiry',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
