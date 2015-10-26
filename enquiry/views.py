fa11b898199c7130e373d627e93c0127bf0e37e1
Patrick Kimber <code@pkimber.net>
2015-10-26 16:13:30 +0000

    'enquiry' view mixin #1178

--- enquiry/views.py
+++ enquiry/views.py
@@ -11,6 +11,17 @@ from base.view_utils import BaseMixin
 from .models import Enquiry
 
 
+class EnquiryCreateMixin:
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update(dict(
+            request=self.request,
+            user=self.request.user,
+        ))
+        return kwargs
+
+
 class EnquiryListView(
         LoginRequiredMixin, StaffuserRequiredMixin, BaseMixin, ListView):
 
